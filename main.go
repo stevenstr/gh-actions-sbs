0f78f19a53c9a456f5191b820fc247aa756f63f3
Stefan Zha <stephenzhavrid@gmail.com>
2025-07-23 12:37:18 +0300

    add simple logger

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
@@ -12,10 +13,12 @@ type Message struct {
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Recieved request at /hello")
 	message := Message{"Hello GH Actions World!"}
 	json.NewEncoder(w).Encode(message)
 }
 func GoodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Recieved request at /goodbye")
 	message := Message{"Goodbye GH Actions World!"}
 	json.NewEncoder(w).Encode(message)
 }
